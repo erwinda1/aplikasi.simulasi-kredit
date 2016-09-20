c2a2df9d0326339023f9dd541c09feeeaf6171d3
dimmaryanto@ukabima <ukabima.dimmaryanto@gmail.com>
2016-09-20 18:58:53 +0700

    update test anual

--- src/test/java/com/hotmail/dimmaryanto/software/TestBungaJenisAnual.java
+++ src/test/java/com/hotmail/dimmaryanto/software/TestBungaJenisAnual.java
@@ -3,51 +3,44 @@ package com.hotmail.dimmaryanto.software;
 import com.hotmail.dimmaryanto.software.model.kredit.jenis.bunga.BungaJenisAnuitas;
 import java.text.NumberFormat;
 import junit.framework.TestCase;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-@Ignore
 public class TestBungaJenisAnual extends TestCase {
 
-    @Ignore
     @Test
     public void testTotalAngsuran() {
         BungaJenisAnuitas f = new BungaJenisAnuitas(1, 20000000.0, 20000000.0, 12, 0.2);
-        assertEquals(1852690.0, f.getTotalAngsuran());
+        assertEquals(1852690, Math.round(f.getTotalAngsuran()));
     }
 
     @Test
     public void testAngsuranBunga() {
         BungaJenisAnuitas f = new BungaJenisAnuitas(1, 20000000.0, 20000000.0, 12, 0.2);
-        assertEquals(333333.0, f.getAngsuranBunga());
+        assertEquals(333333, Math.round(f.getAngsuranBunga()));
 
         f = new BungaJenisAnuitas(2, 20000000.0, 18480643.0, 12, 0.2);
-        assertEquals(308011.0, f.getAngsuranBunga());
+        assertEquals(308011, Math.round(f.getAngsuranBunga()));
     }
 
     @Test
     public void testAngsuranPokok() {
         BungaJenisAnuitas f = new BungaJenisAnuitas(1, 20000000.0, 20000000.0, 12, 0.2);
-        assertEquals(1519357.0, f.getAngsuranPokok());
+        assertEquals(1519357, Math.round(f.getAngsuranPokok()));
 
         f = new BungaJenisAnuitas(2, 20000000.0, 18480643.0, 12, 0.2);
-        assertEquals(1544679.0, f.getAngsuranPokok());
+        assertEquals(1544679, Math.round(f.getAngsuranPokok()));
     }
 
     @Test
     public void testBaki() {
         BungaJenisAnuitas f = new BungaJenisAnuitas(1, 20000000.0, 20000000.0, 12, 0.2);
-        assertEquals(18480643.0, f.getBaki());
+        assertEquals(18480643, Math.round(f.getBaki()));
 
         f = new BungaJenisAnuitas(2, 20000000.0, 18480643.0, 12, 0.2);
-        NumberFormat nf = NumberFormat.getCurrencyInstance();
-        System.out.println(nf.format(f.getBaki()));
-
-        assertEquals(16935964.0, f.getBaki());
-
+        assertEquals(16935964, Math.round(f.getBaki()));
     }
 
 }
